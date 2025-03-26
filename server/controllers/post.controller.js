18613c954559dcd26ffea2cb25fc1451dc2804d6
Abhiraj Patel <patelabhiraj222@gmail.com>
2025-03-26 20:00:07 +0530

    first commit/

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -72,46 +72,6 @@ const getPosts = async (req, res) => {
     }
 }
 
-// const updatePost = async (req, res, next) => {
-//     try {
-//         const { postID, topic, question, answer } = req.body;
-//         console.log(postID, topic, question, answer)
-//         const responseData = await PostModal.findByIdAndUpdate(postID, {
-//             topic,
-//             question,
-//             answer,
-//         }, { new: true });
-//         res.send({
-//             success: true,
-//             responseData,
-//         })
-//     } catch (error) {
-//         console.error("Error :: createPost :: \n" + error)
-//     }
-// }
-
-// const updatePost = async (req, res, next) => {
-//     try {
-//         const { postID, topic, question, answer } = req.body;
-//         if (!postID) return res.status(400).json({ success: false, message: "Post ID is required" });
-
-//         console.log("Updating post:", postID, topic, question, answer); // Debugging
-
-//         const updatedPost = await PostModal.findByIdAndUpdate(
-//             postID,
-//             { topic, question, answer },
-//             { new: true } // Returns updated data
-//         );
-
-//         if (!updatedPost) return res.status(404).json({ success: false, message: "Post not found" });
-
-//         res.json({ success: true, responseData: updatedPost });
-//     } catch (error) {
-//         console.error("Error :: updatePost ::", error);
-//         res.status(500).json({ success: false, message: "Internal Server Error" });
-//     }
-// };
-
 
 const updatePost = async (req, res, next) => {
     try {
